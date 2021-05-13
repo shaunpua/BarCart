a3c4fca93761beeb565b580635fc1a93e137b3d4
shaunpua <svshaunpuax1@gmail.com>
2021-05-13 21:04:15 +0800

    initialize files

---
+++ app/src/main/java/ph/edu/dlsu/s12/barcart/MainActivity.java
@@ -0,0 +1,14 @@
+package ph.edu.dlsu.s12.barcart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
