5fc65a6f85e52bbc89f581a0c41ac3e65c8d4ee8
shaunpua <svshaunpuax1@gmail.com>
2021-05-17 20:36:58 +0800

    created bottom navigation with routes

--- app/src/main/java/ph/edu/dlsu/s12/barcart/menuActivity.java
+++ app/src/main/java/ph/edu/dlsu/s12/barcart/menuActivity.java
@@ -1,8 +1,13 @@
 package ph.edu.dlsu.s12.barcart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class menuActivity extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class menuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListner);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new ScanFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.navScan:
+                            selectedFragment = new ScanFragment();
+                            break;
+                        case R.id.navCart:
+                            selectedFragment = new CartFragment();
+                            break;
+                        case R.id.navInventory:
+                            selectedFragment = new InventoryFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
